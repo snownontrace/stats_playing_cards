e252717b43fd741b30a235da139a16c9a335a292
Shaohe Wang <3818275+snownontrace@users.noreply.github.com>
2020-12-13 00:13:30 -0500

    update bash script

--- push-player-stats-updates.sh
+++ push-player-stats-updates.sh
@@ -2,5 +2,6 @@ python update-player-stats.py
 cd ~/Github/snownontrace.github.io
 git pull origin master
 git add player_stats.md
+git add assets/images/player_history_plot.png
 git commit -m "update player stats"
 git push origin master
